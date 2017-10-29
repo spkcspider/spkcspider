30465b6d7b95d9fb851246f375134b0b3350c036
alex <devkral@web.de>
2017-10-29 18:16:58 +0100

    progress, implement protections partially

--- spkbspider/apps/spiderpk/models.py
+++ spkbspider/apps/spiderpk/models.py
@@ -6,23 +6,25 @@ from django.utils.translation import pgettext_lazy
 from jsonfield import JSONField
 
 # Create your models here.
+from .protections import Protection
 
 class PublicKey(models.Model):
     key = models.TextField(null=True, unique=True)
     # can only be retrieved by hash
-    hash = models.CharField(max_length=512, unique=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    hash = models.CharField(max_length=512, unique=True, null=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False)
     class Meta:
         indexes = [
             models.Index(fields=['hash']),
         ]
 
-
 class UserComponent(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, null=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    data = JSONField(default={})
-    protections = models.ManyToManyField()
+    data = JSONField(default={}, null=False)
+    # should be used
+    assigned = None
+    protections = models.ManyToManyField(Protection, through=AssignedProtection, limit_choices_to=Protection.objects.valid())
     class Meta:
         unique_together = [("user", "name"),]
         indexes = [
