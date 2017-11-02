17f48847d873b44df31a7c568e3a1379fc6900df
alex <devkral@web.de>
2017-11-02 20:17:17 +0100

    use update view

--- spkbspider/apps/spideraccounts/models.py
+++ spkbspider/apps/spideraccounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse_lazy
 
 # Create your models here.
 
@@ -8,3 +9,6 @@ class SpiderUser(AbstractUser):
     REQUIRED_FIELDS = []
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
+
+    #def get_absolute_url(self):
+    #    return reverse_lazy()
--- spkbspider/apps/spideraccounts/views.py
+++ spkbspider/apps/spideraccounts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, LoginView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth import get_user_model
 
 import time
 from .forms import SignupForm, UserUpdateForm
@@ -39,9 +40,12 @@ class UserLoginView(LoginView):
         time.sleep(1.5)
         return super().post(request, *args, **kwargs)
 
-class UserUpdateView(LoginRequiredMixin, FormView):
+class UserUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'registration/profile.html'
     form_class = UserUpdateForm
+    def get_object(self, queryset=None):
+        obj = get_user_model().objects.get(id=self.request.user.pk)
+        return obj
 
 class RecoverView(PasswordResetView):
     success_url = reverse_lazy('auth:password_reset_confirm')
