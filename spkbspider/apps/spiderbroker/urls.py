231cb871d25b1a2e6084fc140a1d485abf9f0b22
alex <devkral@web.de>
2017-11-02 19:41:35 +0100

    some fixes for brokers

--- spkbspider/apps/spiderbroker/urls.py
+++ spkbspider/apps/spiderbroker/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
+from django.urls import reverse
 
+from django.views.generic.base import RedirectView
 from .views import BrokerIndex, BrokerDetail, BrokerCreate, BrokerDelete
+
+class RedirectUser(RedirectView):
+    permanent = True
+    def get_success_url(self):
+        return reverse("broker:bk-list", user=request.user)
 
 # pk = PublicKey, uc = UserComponent
 urlpatterns = [
@@ -8,5 +15,6 @@ urlpatterns = [
     url(r'^bk/(?P<user>[-\w]+)/create/$', BrokerCreate.as_view(), name='bk-create'),
     url(r'^bk/(?P<user>[-\w]+)/view/(?P<hash>[-\w]+)/$', BrokerDetail.as_view(), name='bk-view'),
     url(r'^bk/(?P<user>[-\w]+)/delete/(?P<hash>[-\w]+)/$', BrokerDelete.as_view(), name='bk-delete'),
+    url(r'^bk/$', RedirectUser.as_view()),
 
 ]
