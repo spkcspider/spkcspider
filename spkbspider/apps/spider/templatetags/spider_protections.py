59799a1ef3940c1e73588d9524e698a954e04404
alex <devkral@web.de>
2017-12-04 17:46:25 +0100

    fix urls, add template tags for use with protections

---
+++ spkbspider/apps/spider/templatetags/spider_protections.py
@@ -0,0 +1,42 @@
+from django import template
+from django.urls import resolve
+from django.core.exceptions import Resolver404, ObjectDoesNotExist
+
+from ..models import AssignedProtection, UserComponent
+from ..views import ContentView, ComponentIndex, ContentIndex
+from ..protections import installed_protections
+
+register = template.Library()
+
+protected_views = [ContentView, ContentIndex, ComponentIndex]
+# includes recovery because recovery should NEVER be included in a normal view query
+unavailable_components_for_use = ["recovery", "index"]
+unavailable_components_for_list = ["index"]
+
+@register.simple_tag(takes_context=True)
+def use_protections(context):
+    try:
+        res = resolve(context["request"].path)
+        if res.func.view_class not in protected_views: return False
+        # in case of ComponentIndex None is used so protections work
+        # in case of ContentView index is specified so unavailable
+        if res.kwargs.get("name", None) in unavailable_components_for_use: return False
+        return True
+    except Resolver404:
+        return False
+
+
+@register.simple_tag(takes_context=True)
+def list_protections(context, path=None):
+    if not path:
+        path = context["request"].path
+    try:
+        res = resolve(path)
+        if res.kwargs.get("name", None) in unavailable_components_for_list:
+            return AssignedProtection.objects.none()
+        usercomponent = UserComponent.objects.get(
+            user__username=res.kwargs["user"],
+            name=res.kwargs.get("name", "index"))
+        return usercomponent.assigned.prefetch_related('protection').filter(protection__code__in=installed_protections)
+    except (KeyError, Resolver404, ObjectDoesNotExist):
+            return AssignedProtection.objects.none()
--- spkbspider/apps/spider/urls.py
+++ spkbspider/apps/spider/urls.py
@@ -7,13 +7,13 @@ app_name = "spiderucs"
 
 # uc = UserComponent
 urlpatterns = [
-    path('<slug:user>/', ComponentIndex.as_view(), name='ucomponent-list'),
+    path('<slug:user>/index/', ComponentIndex.as_view(), name='ucomponent-list'),
     path('<slug:user>/create/', ComponentCreate.as_view(), name='ucomponent-create'),
     path('<slug:user>/update/<slug:name>/', ComponentUpdate.as_view(), name='ucomponent-update'),
     path('<slug:user>/delete-reset/<slug:name>/', ComponentResetDelete.as_view(), name='ucomponent-resetdelete'),
     path('<slug:user>/delete/<slug:name>/', ComponentDelete.as_view(), name='ucomponent-delete'),
 
-    path('<slug:user>/list/<slug:name>', ContentIndex.as_view(), name='ucontent-list'),
+    path('<slug:user>/list/<slug:name>/', ContentIndex.as_view(), name='ucontent-list'),
     path('<slug:user>/view/<slug:name>/<int:id>/', ContentView.as_view(), name='ucontent-view'),
     path('<slug:user>/add/<slug:name>/<slug:type>/', ContentAdd.as_view(), name='ucontent-add'),
     path('<slug:user>/cupdate/<slug:name>/<int:id>/', ContentUpdate.as_view(), name='ucontent-update'),
