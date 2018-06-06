6345739cb9204dd29320821fa1713bc5185bb39c
alex <devkral@web.de>
2018-06-06 17:34:52 +0200

    recreate db, only migration creates protection, design

--- spkcspider/apps/spider/apps.py
+++ spkcspider/apps/spider/apps.py
@@ -12,9 +12,7 @@ class SpiderBaseConfig(AppConfig):
         from .signals import (
             InitProtectionsCallback, InitUserComponentsCallback
         )
-        from .protections import initialize_protection_models
         post_save.connect(InitUserComponentsCallback, sender=get_user_model(),
                           dispatch_uid="initial_usercomponents")
         post_migrate.connect(InitProtectionsCallback,
                              dispatch_uid="update_protections")
-        initialize_protection_models()
