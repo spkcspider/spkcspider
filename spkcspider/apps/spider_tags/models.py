c30804bcce753b396f21d0deccde46dcc2ea208c
alex <devkral@web.de>
2018-06-13 00:52:08 +0200

    form generator

--- spkcspider/apps/spider_tags/models.py
+++ spkcspider/apps/spider_tags/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from urllib.parse import urlsplit
+from importlib import import_module
 
+from django import forms
 
 from jsonfield import JSONField
 
@@ -10,6 +11,38 @@ from spkcspider.apps.spider.contents import BaseContent, add_content
 # Create your models here.
 
 
+def order_fields(base, layout):
+    ret = []
+    for i in layout:
+        split = list(i.items())[0]
+        if isinstance(split[1], list):
+            ret.append(order_fields(
+                base.get(split[0], {}), split[1]
+            ))
+        else:
+            ret.append(base.get(split[0]))
+    return ret
+
+
+def generate_fields(layout, base=None, prefix=""):
+    ret = []
+    for i in layout:
+        split = list(i.items())[0]
+        if isinstance(split[1], list):
+            ret + generate_fields(
+                split[1], prefix="%s_%s" % (prefix, split[0]),
+                base=base.get(split[0], {}) if base else None
+            )
+        elif isinstance(split[1], dict):
+            d = split[1].copy()
+            field = getattr(forms, d.pop("field"))
+            ret.append(field(initial=base.get(split[0]), **d))
+        else:
+            field = getattr(forms, split[1])
+            ret.append(field(initial=base.get(split[0])))
+    return ret
+
+
 class TagType(models.Model):
     layout = JSONField(default=[])
     default_verifiers = JSONField(default=[])
@@ -17,9 +50,11 @@ class TagType(models.Model):
 
 @add_content
 class SpiderTag(BaseContent):
-    tagtype = models.ForeignKey(TagType)
+    tagtype = models.ForeignKey(
+        TagType, related_name="tags", on_delete=models.PROTECT
+    )
     tagdata = JSONField(default={})
-    verfied_by = JSONField(default={})
+    verfied_by = JSONField(default=[])
 
-    def __str__(self):
-        return urlsplit(self.url).netloc
+    def get_ordered(self):
+        return order_fields(self.tagdata, self.tagtype.layout)
