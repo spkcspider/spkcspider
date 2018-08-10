e2d25bcadc5a6273d149d23c867a5f171bd7e255
alex <devkral@web.de>
2018-08-10 15:29:01 +0200

    fix create_superuser with optional email

--- spkcspider/apps/spider_accounts/models.py
+++ spkcspider/apps/spider_accounts/models.py
@@ -1,4 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as CoreUserManager
+
+
+class UserManager(CoreUserManager):
+    def create_superuser(self, username, password, email=None, **extra_fields):
+        # fix users with optional email
+        super().create_superuser(username, email, password, **extra_fields)
+
 
 # Create your models here.
 
@@ -6,6 +14,8 @@ from django.contrib.auth.models import AbstractUser
 class SpiderUser(AbstractUser):
     REQUIRED_FIELDS = []
     SAFE_FIELDS = ['username', 'email', 'password']
+
+    objects = UserManager()
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
