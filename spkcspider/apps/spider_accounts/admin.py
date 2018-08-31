f5da6c44d1a2978561f1f27425e9e6f2ba34910c
alex <devkral@web.de>
2018-08-31 19:16:33 +0200

    add admin for users

--- spkcspider/apps/spider_accounts/admin.py
+++ spkcspider/apps/spider_accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import admin as user_admin
 
+from .models import SpiderUser
 # Register your models here.
+
+
+@admin.register(SpiderUser)
+class UserAdmin(user_admin.UserAdmin):
+    pass
