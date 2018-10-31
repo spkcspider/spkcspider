ed50ea513acd727d8d699c590546c2f7aca79c99
alex <devkral@web.de>
2018-10-31 17:34:55 +0100

    fixes

--- spkcspider/apps/verifier/views.py
+++ spkcspider/apps/verifier/views.py
@@ -1,7 +1,6 @@
 
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
-from django.utils.translation import gettext as _
 from django.http import HttpResponseRedirect
 
 from .models import DataVerificationTag
@@ -14,13 +13,7 @@ class CreateEntry(CreateView):
     form_class = CreateEntryForm
     template_name = "spider_verifier/dv_form.html"
 
-    def get_context_data(self, **kwargs):
-        kwargs["legend"] = _("Send spider or content to verify")
-        kwargs["confirm"] = _("Send")
-        return super().get_context_data(**kwargs)
-
     def form_invalid(self, form):
-        print(form.errors, form.fields["hash"].initial)
         q = DataVerificationTag.objects.filter(
             hash=form.fields["hash"].initial
         ).first()
