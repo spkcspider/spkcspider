662ca2d4807220dcb49b61a5298d1460a435036d
alex <devkral@web.de>
2018-11-18 23:33:44 +0100

    update tests

--- tests/test_spider.py
+++ tests/test_spider.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.test import Client
+from rdflib import Graph
 
 from spkcspider.apps.spider_accounts.models import SpiderUser
-from spkcspider.apps.spider.models import UserComponent
+from spkcspider.apps.spider.constants.static import spkcgraph
 # Create your tests here.
 
 
@@ -12,9 +13,6 @@ class ComponentTest(TestCase):
         self.user = SpiderUser.objects.create_user(
             username="testuser1", password="abc"
         )
-        UserComponent.objects.create(
-            user=self.user, name="public", public=True
-        )
 
     def test_index_creation(self):
         self.assertTrue(self.user.usercomponent_set.get(name="index"))
@@ -22,8 +20,16 @@ class ComponentTest(TestCase):
             self.user.usercomponent_set.create(name="index")
 
     def test_glob_index(self):
+        self.user.usercomponent_set.create(name="test1", public=True)
+        self.user.usercomponent_set.create(name="test2", public=True)
+        self.user.usercomponent_set.create(name="test3", public=True)
         # Issue a GET request.
-        response = self.client.get('/spider/ucs/')
+        response = self.client.get('/spider/ucs/?raw=true')
 
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
+        g = Graph()
+        g.parse(response, format="turtle")
+        self.assertLength(list(
+            g.triples((None, spkcgraph["components"], None))
+        ), 4)
