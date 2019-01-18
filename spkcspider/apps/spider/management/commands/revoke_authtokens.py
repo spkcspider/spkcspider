e7942b16b0fd06297921d3aaf01e9a410226e64f
alex <devkral@web.de>
2019-01-18 16:27:53 +0100

    allow specifying persistent tokens

--- spkcspider/apps/spider/management/commands/revoke_authtokens.py
+++ spkcspider/apps/spider/management/commands/revoke_authtokens.py
@@ -18,19 +18,42 @@ class Command(BaseCommand):
             '--oldest', action='store', dest='oldest', default=None,
             help='Delete oldest x tokens',
         )
+        parser.add_argument(
+            '--referrer', action='store', dest='referrer', default=None,
+            help='Delete tokens of referrer',
+        )
+        parser.add_argument(
+            '--persist', action='store', dest='anchor', default=None,
+            help='Delete tokens of anchor id, with "all": persistent tokens',
+        )
 
-    def handle(self, **options):
+    def handle(self, oldest=None, days=None, anchor=None, **options):
         from spkcspider.apps.spider.models import AuthToken
-        q = AuthToken.objects.filter(persist=-1)
-        if options['days']:
+        q = AuthToken.objects.all()
+
+        if options['referrer']:
+            q = q.filter(referrer=options["referrer"])
+            if anchor == "all":
+                pass
+            elif anchor:
+                q = q.filter(persist=int(anchor))
+            else:
+                q = q.filter(persist__gte=0)
+        elif anchor == "all":
+            pass
+        elif anchor:
+            q = q.filter(persist=int(anchor))
+        else:
+            q = q.filter(persist=-1)
+        if days:
             dt = datetime.datetime.now() - \
-                datetime.timedelta(days=int(options['days']))
+                datetime.timedelta(days=int(days))
             q.filter(created__lt=dt)
-        if options['oldest']:
+        if oldest:
             # create list with ids
             ids = q.order_by("created").values_list(
                 'id', flat=True
-            )[:int(options['oldest'])]
+            )[:int(oldest)]
             # recreate Query
             q = AuthToken.objects.filter(id__in=ids)
         print(q.delete()[0])
