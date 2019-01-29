42c2e5763dda96bb04842e672132d53255589613
alex <devkral@web.de>
2019-01-29 02:04:46 +0100

    correct comment

--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
+        # exceptions
         try:
             import django  # noqa: F401
         except ImportError:
