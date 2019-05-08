504926007b43056e41643a046d2036b074245c88
alex <devkral@web.de>
2019-05-08 22:36:27 +0200

    spider_filet text: fix description helper

--- spkcspider/apps/spider_filets/static/spider_filets/description_helper.js
+++ spkcspider/apps/spider_filets/static/spider_filets/description_helper.js
@@ -1,4 +1,7 @@
 
+let clean_description_text = function(inp){
+  return inp.replace(/(?:<|&lt;)\/?.(?!&gt;|>)*.?(?:&gt;|>|$)/g, " ").replace(/\ +/g, " ").trim();
+}
 
 document.addEventListener("DOMContentLoaded", function(){
   let source_element = $("#id_text");
@@ -6,13 +9,13 @@ document.addEventListener("DOMContentLoaded", function(){
   let modify_dest = false;
   /* last char will be replaced by …*/
   let max_length = dest_element.attributes.maxlength.value;
-  let val = source_element.html().replace(/<\/?[^>]+(>|$)/g, " ").replace(/\ +/g, " ").trim().substr(0,max_length);
+  let val = clean_description_text(source_element.html()).substr(0,max_length);
   if (dest_element.value == "" || dest_element.value.substr(0,max_length)==val){
     modify_dest = true;
     dest_element.value = val;
   }
   dest_element.addEventListener("input", function(event){
-    val = source_element.html().replace(/<\/?[^>]+(>|$)/g, " ").replace(/\ +/g, " ").trim().substr(0,max_length);
+    val = clean_description_text(source_element.html()).substr(0,max_length);
     /* || dest_element.value.substr(0,500)==val*/
     if(event.target.value==""){
       modify_dest = true;
@@ -23,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function(){
   });
   source_element.on("tbwchange", function(event){
     if (modify_dest){
-      dest_element.value = event.target.value.replace(/<\/?[^>]+(>|$)/g, " ").replace(/\ +/g, " ").trim().substr(0,max_length);
+      dest_element.value = clean_description_text(event.target.value).substr(0,max_length);
     }
   });
 });
