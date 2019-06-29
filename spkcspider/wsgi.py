38d7c3478b9c3960d4ffe9547435ec014a4f5d10
alex <devkral@web.de>
2019-06-29 20:33:23 +0200

    update comment

--- spkcspider/wsgi.py
+++ spkcspider/wsgi.py
@@ -3,8 +3,6 @@ WSGI config for spkcspider project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
